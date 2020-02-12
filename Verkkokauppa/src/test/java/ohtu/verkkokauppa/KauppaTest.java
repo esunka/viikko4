0ac27f07b7ddcea20ab0e79b39769493b6daae62
antton <antton.kortelainen@gmail.com>
2020-02-12 20:45:27 +0200

    4.4.

--- Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -3,6 +3,7 @@ package ohtu.verkkokauppa;
 import org.junit.Before;
 import org.junit.Test;
 
+import ohtu.verkkokauppa.Kauppa;
 import ohtu.verkkokauppa.Viitegeneraattori;
 
 import static org.junit.Assert.*;
@@ -95,6 +96,57 @@ public class KauppaTest {
 		k.tilimaksu("pekka", "12345");
 		verify(pankki).tilisiirto(eq("pekka"),anyInt(),eq("12345"),anyString(),eq(5));
 
+	}
+
+	@Test
+	public void checkReset() {
+		when(viite.uusi()).thenReturn(42);
+		when(varasto.saldo(1)).thenReturn(10); 
+		when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+		k.aloitaAsiointi();
+		k.lisaaKoriin(1);
+		k.tilimaksu("pekka", "12345");
+        verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(),anyInt());   
+	
+		k.aloitaAsiointi();
+		k.lisaaKoriin(1);
+		k.tilimaksu("foo", "55555");
+
+		verify(pankki).tilisiirto(eq("foo"), anyInt(), eq("55555"), anyString(),eq(5));   
+	
+	}
+
+	@Test
+	public void refNumber() {
+		when(viite.uusi()).thenReturn(42).thenReturn(43);
+        when(varasto.saldo(1)).thenReturn(10); 
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+        verify(pankki).tilisiirto(anyString(), eq(42), anyString(), anyString(),anyInt());   
+	
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+        verify(pankki).tilisiirto(anyString(), eq(43), anyString(), anyString(),anyInt());   
+	
+	}
+
+	@Test
+	public void removeCart() {
+
+        when(viite.uusi()).thenReturn(42);
+		when(varasto.saldo(1)).thenReturn(10); 
+		Tuote t = new Tuote(1, "maito", 5);
+        when(varasto.haeTuote(1)).thenReturn(t);
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+		k.poistaKorista(1);
+		k.tilimaksu("pekka", "12345");
+        verify(varasto,times(1)).palautaVarastoon(t);   
+	
 	}
 }
 
